3eb79e45b10c6e607feec92e1aae14e23113f78b
Andy Chong Chin Shin <andychong2@gmail.com>
2015-09-06 15:17:31 +0800

    setting up gradle and antlr4

--- src/main/java/com/sqatntu/stylechecker/Main.java
+++ src/main/java/com/sqatntu/stylechecker/Main.java
@@ -1,5 +1,7 @@
 package com.sqatntu.stylechecker;
 
+import com.sqatntu.stylechecker.JavaLexer;
+
 /**
  * Main class for Style Checker
  */
@@ -7,5 +9,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println(JavaLexer.ABSTRACT);
     }
 }
