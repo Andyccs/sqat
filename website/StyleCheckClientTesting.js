c7395052ebae152b970ff112e401ea1e2665b885
Andy Chong Chin Shin <andychong2@gmail.com>
2015-09-12 01:03:40 +0800

    configure eslintrc for the project

--- website/StyleCheckClientTesting.js
+++ website/StyleCheckClientTesting.js
@@ -1,27 +1,27 @@
-var PROTO_PATH = '../stylechecker/src/main/proto/style_check.proto'
+var PROTO_PATH = '../stylechecker/src/main/proto/style_check.proto';
 
 var grpc = require('grpc');
-var style_check_proto = grpc.load(PROTO_PATH).stylechecker;
+var styleCheckProto = grpc.load(PROTO_PATH).stylechecker;
 
-function main() {
-    var client = new style_check_proto.StyleCheck(
-        'localhost:50051',
-        grpc.Credentials.createFake());
+function main()     {
+  var client = new styleCheckProto.StyleCheck(
+    'localhost:50051',
+    grpc.Credentials.createFake());
 
-    var filePath = "src/test/resources/MethodNameFormatCamelCase.java";
-    var configFilePath = "src/test/resources/MethodNameFormatListenerConfig.json";
-    var request = {
-        filePath: filePath,
-        configurationFilePath: configFilePath
-    };
+  var filePath = 'src/test/resources/MethodNameFormatCamelCase.java';
+  var configFilePath = 'src/test/resources/MethodNameFormatListenerConfig.json';
+  var request = {
+    filePath: filePath,
+    configurationFilePath: configFilePath
+  };
 
-    client.check(request, function(err, response) {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.log(response);
-    });
+  client.check(request, function(err, response) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(response);
+  });
 }
 
 main();
