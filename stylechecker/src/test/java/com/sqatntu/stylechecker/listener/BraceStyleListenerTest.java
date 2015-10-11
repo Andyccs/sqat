98f0ac2cc11bde705d1e4bb3d476ecdc580478be
Andy Chong Chin Shin <andychong2@gmail.com>
2015-10-11 21:47:32 +0800

    refactor ListenerTest class

--- stylechecker/src/test/java/com/sqatntu/stylechecker/listener/BraceStyleListenerTest.java
+++ stylechecker/src/test/java/com/sqatntu/stylechecker/listener/BraceStyleListenerTest.java
@@ -1,57 +1,21 @@
 package com.sqatntu.stylechecker.listener;
 
-import static org.junit.Assert.assertEquals;
-
+import com.sqatntu.stylechecker.ListenerTest;
 import com.sqatntu.stylechecker.StyleChecker;
 import com.sqatntu.stylechecker.StyleCheckerException;
 import com.sqatntu.stylechecker.TestCode;
 import com.sqatntu.stylechecker.TestConfig;
-import com.sqatntu.stylechecker.TestUtil;
 import com.sqatntu.stylechecker.report.StyleReport;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TestName;
 
-import java.io.File;
 import java.io.IOException;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by andyccs on 11/10/15.
  */
-public class BraceStyleListenerTest {
-
-  @Rule
-  public TestName name = new TestName();
-
-  private String testCode;
-  private String testConfig;
-
-  @Before
-  public void before() throws NoSuchMethodException, IOException {
-    Method method = this.getClass().getMethod(name.getMethodName());
-    Annotation[] annotations = method.getDeclaredAnnotations();
-
-    for(Annotation annotation : annotations) {
-      if(annotation instanceof TestCode){
-        TestCode testCodeAnnotation = (TestCode) annotation;
-        testCode = TestUtil.loadFile(testCodeAnnotation.fileName());
-      }
-      else if(annotation instanceof TestConfig) {
-        TestConfig testConfigAnnotation = (TestConfig) annotation;
-        testConfig = TestUtil.loadFile(testConfigAnnotation.fileName());
-      }
-    }
-  }
-
-  @After
-  public void after() {
-    testCode = null;
-    testConfig = null;
-  }
+public class BraceStyleListenerTest extends ListenerTest {
 
   @Test
   @TestCode(detail = "krStyleCode", fileName = "src/test/resources/BraceKRStyle.java")
