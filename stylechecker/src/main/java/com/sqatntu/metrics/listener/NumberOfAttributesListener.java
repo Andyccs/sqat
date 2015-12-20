42b5ff89e5a9b8ccde5a73cf0423364193543ff8
Andy Chong Chin Shin <andychong2@gmail.com>
2015-12-20 18:30:21 +0800

    calculate number of attributes in a class

---
+++ stylechecker/src/main/java/com/sqatntu/metrics/listener/NumberOfAttributesListener.java
@@ -0,0 +1,29 @@
+package com.sqatntu.metrics.listener;
+
+import com.sqatntu.api.JavaBaseListener;
+import com.sqatntu.api.JavaParser;
+import com.sqatntu.metrics.report.MetricReport;
+import org.antlr.v4.runtime.RuleContext;
+
+import java.util.List;
+
+/**
+ * Created by andyccs on 20/12/15.
+ */
+public class NumberOfAttributesListener extends JavaBaseListener{
+
+  private MetricReport report;
+
+  public NumberOfAttributesListener(MetricReport report) {
+    this.report = report;
+  }
+
+  @Override
+  public void enterFieldDeclaration(JavaParser.FieldDeclarationContext ctx) {
+    List<JavaParser.VariableDeclaratorContext> variableDeclaratorContexts = ctx
+        .getRuleContext(JavaParser.VariableDeclaratorsContext.class, 0)
+        .getRuleContexts(JavaParser.VariableDeclaratorContext.class);
+
+    report.addNumberOfAttributes(variableDeclaratorContexts.size());
+  }
+}
